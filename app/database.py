04049adb8f761361c446262e565769d58d0b36e8
d33sty <distortion616@gmail.com>
2026-05-15 12:02:22 +0300

    15.05.2026 feat: sens. info protect

--- app/database.py
+++ app/database.py
@@ -18,7 +18,18 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, Asyn
 from sqlalchemy.orm import DeclarativeBase
 
 # Строка подключения для PostgreSQl
-DATABASE_URL = "postgresql+asyncpg://ecommerce_user:admin@localhost:5434/ecommerce_db"
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+DB_USER = os.getenv("DB_USER", "postgres")
+DB_PASS = os.getenv("DB_PASS", "")
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "5432")
+DB_NAME = os.getenv("DB_NAME", "postgres")
+
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 # Создаём Engine
 async_engine = create_async_engine(DATABASE_URL, echo=True)
