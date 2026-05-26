66b2fa45d6a4a429780a8b8eac8d397d28f6bdf6
d33sty <distortion616@gmail.com>
2026-05-26 15:19:14 +0300

    26.05.2026 fix: new migration

--- app/migrations/versions/d1e2f3a4b5c6_increase_product_name_description_length.py
+++ app/migrations/versions/d1e2f3a4b5c6_increase_product_name_description_length.py
@@ -9,6 +9,7 @@ from typing import Sequence, Union
 
 from alembic import op
 import sqlalchemy as sa
+import sqlalchemy.dialects.postgresql
 
 
 revision: str = 'd1e2f3a4b5c6'
@@ -17,11 +18,22 @@ branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
+TSV_EXPR = """
+    setweight(to_tsvector('russian', coalesce(name, '')), 'A')
+    ||
+    setweight(to_tsvector('russian', coalesce(description, '')), 'B')
+"""
+
+
 def upgrade() -> None:
+    op.drop_column('products', 'tsv')
     op.alter_column('products', 'name', type_=sa.String(255), existing_nullable=False)
     op.alter_column('products', 'description', type_=sa.String(5000), existing_nullable=True)
+    op.add_column('products', sa.Column('tsv', sa.dialects.postgresql.TSVECTOR(), sa.Computed(TSV_EXPR, persisted=True), nullable=False))
 
 
 def downgrade() -> None:
+    op.drop_column('products', 'tsv')
     op.alter_column('products', 'name', type_=sa.String(100), existing_nullable=False)
     op.alter_column('products', 'description', type_=sa.String(500), existing_nullable=True)
+    op.add_column('products', sa.Column('tsv', sa.dialects.postgresql.TSVECTOR(), sa.Computed(TSV_EXPR, persisted=True), nullable=False))
